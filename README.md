2128600e376f5cd0d4fc663ef48b2b4aecbab986
MatoH <matejhusar@gmail.com>
2020-03-07 14:37:06 +0100

    Finished fetching and parsing process of rssHemnetUrl. Updated readme instructions

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Hemnet webscraper notifier
 Node app scraping Hemnet data and sending notifications
 
-## How to configure
+## Prerequisites
+* Register at [Hemnet](https://www.hemnet.se/)
+* Search for an accomodation, set filters etc..
+* Enter "[saved searched](https://www.hemnet.se/mitt_hemnet/sparade_sokningar)"
+* Copy RSS source for scraping data - RSS_HEMNET_URL
+
+## How to start on local environment
+```js
+RSS_HEMNET_URL=https://www.hemnet.se/mitt_hemnet/sparade_sokningar/xxxxxxxxx.xml npm run start
+```
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const axios = require('axios');
+const xml2js = require('xml2js');
+
 const app = express();
 const port = process.env.PORT || 3000;
+const rssHemnetUrl = process.env.RSS_HEMNET_URL;
+
 app.get('/', (req, res) => {
-   res.send('Hello World');
+    axios.get(rssHemnetUrl)
+        .then(response => xml2js.parseStringPromise(response.data))
+        .then((parsedResponse) => {
+           const jsonResponse = JSON.stringify(parsedResponse);
+           res.send(jsonResponse)
+        });
 });
 
-app.listen(port, () => console.log(`App is listening on port ${port}, \nAccess app: http://localhost:${port}`));
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}, \nAccess app: http://localhost:${port}`)
+});
